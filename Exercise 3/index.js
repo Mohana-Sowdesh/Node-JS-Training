8f230ef6af0ccdccef8f9d423d70513ff65144e0
Mohana-Sowdesh <mohanasowdesh.ramadoss@cdw.com>
2023-04-03 09:51:41 +0530

    Added new global module, created promise

--- Exercise 3/index.js
+++ Exercise 3/index.js
@@ -1,18 +1,48 @@
 import http from 'http';
 import ora from 'ora';
+import validator from 'validator';
 
-http.createServer((req, res, err) => {
-    res.write("Sirius... A CDW Company...");
+function timeout() {
+    return new Promise((resolve, reject) => {
+        const spinner = ora('Loading unicorns').start();
+        try {
+            setTimeout(() => {
+                resolve(spinner.color = 'green');
+                resolve(spinner.text = 'Loading stars');
+                resolve(spinner.suffixText = 'Suffix Text');
+                resolve(spinner.prefixText = 'Prefix Text');
+                resolve(spinner.indent = 5);
+            }, 5000);
+        }
+        catch(e) {
+            reject(console.log("Some error!!"));
+        }
+    });
+}
 
-    const spinner = ora('Loading unicorns').start();
+async function validatingDetails() {
+    //isEmail()
+    console.log("Is sowdesh@gmail.com an email: "+validator.isEmail('sowdesh@gmail.com'));
+    console.log("Is sowdesh_gmail.com an email: "+validator.isEmail('sowdesh_gmail.com'));
 
-    setTimeout(() => {
-        spinner.color = 'green';
-        spinner.text = 'Loading stars';
-        spinner.suffixText = 'Suffix Text';
-        spinner.prefixText = 'Prefix Text';
-        spinner.indent = 5;
-    }, 5000);
+    //equals()
+    console.log("Are  Sowdesh and sowdesh equal: "+validator.equals('Sowdesh','sowdesh'));
+    console.log("Are  Sowdesh and Sowdesh equal: "+validator.equals('Sowdesh','Sowdesh'));
 
-    res.end();
-}).listen(4000);
+    //isAlpha()
+    console.log("Is A an alphabet: "+validator.isAlpha('A'));
+    console.log("Is 7 an alphabet: "+validator.isAlpha('7'));
+
+    //contains()
+    console.log("Does 'StrIng' contains 'ing' ignoring case: "+validator.contains("StrIng","ing",{ignoreCase: true}));
+    console.log("Does 'StrIng' contains 'ing' without ignoring case: "+validator.contains("StrIng","ing",{ignoreCase: false}));
+
+    //isStrongPassword()
+    console.log("Is Uhshs726$ a strong password: "+validator.isStrongPassword("Uhshs726$"));
+    console.log("Is hshs726$ a strong password: "+validator.isStrongPassword("hshs726$"));
+
+    let data = await timeout();
+}
+validatingDetails();
+
+
